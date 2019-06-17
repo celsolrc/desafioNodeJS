b678e0a5155eb4d8f7f65ae6fae80084d454f166
Celso Cruz <celsolrc@hotmail.com>
2019-06-17 06:06:24 -0300

    conexao com MongoDB Atlas

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/desafionode', { useNewUrlParser:true, useCreateIndex:true});
+//const strconn = 'mongodb://localhost/desafionode';
+const strconn = 'mongodb+srv://desafionode:testenode@cluster0-2vbef.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(strconn, { useNewUrlParser:true, useCreateIndex:true});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
