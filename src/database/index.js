03126b7a47d22b5d1e6e39bfc793702b507adc20
Celso Cruz <celsolrc@hotmail.com>
2019-06-17 07:02:02 -0300

    De volta ao mongo atlas

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 //const strconn = 'mongodb://localhost/desafionode';
-//const strconn = 'mongodb+srv://desafionode:testenode@cluster0-2vbef.mongodb.net/test?retryWrites=true&w=majority';
-const strconn = 'mongodb://desafionode:testenode1@ds139037.mlab.com:39037/demonode';
+const strconn = 'mongodb+srv://desafionode:testenode@cluster0-2vbef.mongodb.net/test?retryWrites=true&w=majority';
+//const strconn = 'mongodb://desafionode:testenode1@ds139037.mlab.com:39037/demonode';
 
 mongoose.connect(strconn, { useNewUrlParser:true, useCreateIndex:true});
 mongoose.Promise = global.Promise;
