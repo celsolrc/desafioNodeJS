9855e61018d45bdc0061a304985aedd8775168fd
Celso Cruz <celsolrc@hotmail.com>
2019-06-17 21:10:16 -0300

    Usando clever cloud

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 //const strconn = 'mongodb://localhost/desafionode';
 //const strconn = 'mongodb+srv://desafionode:testenode@cluster0-2vbef.mongodb.net/test?retryWrites=true&w=majority';
-const strconn = 'mongodb://desafionode:testenode1@ds139037.mlab.com:39037/demonode';
+//const strconn = 'mongodb://desafionode:testenode1@ds139037.mlab.com:39037/demonode';
+const strconn = 'mongodb://uzk0jxponfxwyhcd1rlv:RW4VGugBk5VOnBFgj5t6@bhoxzuvhnqsbu4i-mongodb.services.clever-cloud.com:27017/bhoxzuvhnqsbu4i';
 
 mongoose.connect(strconn, { useNewUrlParser:true, useCreateIndex:true});
 mongoose.Promise = global.Promise;
